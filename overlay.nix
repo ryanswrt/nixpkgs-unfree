5bb307fceba72670d4ebc7423eb0a51c91c35399
Ryan Swart <ryan@talo.systems>
2023-10-09 11:45:18 +0800

    fix: remove extra unfree-overrides

--- overlay.nix
+++ overlay.nix
@@ -2,37 +2,21 @@
 final: prev: {
   cudatoolkit = prev.cudatoolkit_11;
 
-  mpich = prev.mpich.override {
-    ch4backend = final.ucx;
-  };
+  openmpi = prev.openmpi.override { cudaSupport = true; };
 
-  openmpi = prev.openmpi.override {
-    cudaSupport = true;
-  };
-
-  ucx = prev.ucx.override {
-    enableCuda = true;
-  };
-
-  blas = prev.blas.override {
-    blasProvider = final.mkl;
-  };
-
-  lapack = prev.lapack.override {
-    lapackProvider = final.mkl;
-  };
+  ucx = prev.ucx.override { enableCuda = true; };
 
   # Add packages here that we want to build and that are not
   # unfree+redistributable.
-  extraChecks = [
-    "blas"
-    "cudatoolkit"
-    "lapack"
-    "mpich"
-    "openmpi"
-    "python3Packages.jaxlibWithCuda"
-    "python3Packages.tensorflowWithCuda"
-    "python3Packages.pytorchWithCuda"
-    "ucx"
-  ];
+  # extraChecks = [
+  #   "blas"
+  #   "cudatoolkit"
+  #   "lapack"
+  #   "mpich"
+  #   "openmpi"
+  #   "python3Packages.jaxlibWithCuda"
+  #   "python3Packages.tensorflowWithCuda"
+  #   "python3Packages.pytorchWithCuda"
+  #   "ucx"
+  # ];
 }
